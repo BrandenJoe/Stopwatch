0e74b76930f57608098507783d8c37b50caa3e71
BrandenJoe <147530777+BrandenJoe@users.noreply.github.com>
2025-02-26 20:30:46 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,27 +1,55 @@
-const timerMilliseconds = document.querySelector('timer__milliseconds');
-const timerSeconds = document.querySelector('timer__seconds');
-const timerMinutes = document.querySelector('timer__minutes');
+const timerMilliseconds = document.querySelector('.timer__milliseconds');
+const timerSeconds = document.querySelector('.timer__seconds');
+const timerMinutes = document.querySelector('.timer__minutes');
 
 let cancelId;
-let startTime
+let startTime;
+let savedTime = 0;
+
 function startTimer(){
  startTime = Date.now();
  cancelId = requestAnimationFrame(updateTimer);
 }
 function stopTimer(){
+
+ savedTime = savedTime + Date.now() - startTime;
     cancelAnimationFrame(cancelId);
 }
 
 function resetTimer(){
+startTime = Date.now();
+savedTime = 0;
 
+timerMilliseconds.innerHTML ='000';
+timerSeconds.innerHTML = '00'
+timerMinutes.innerHTML = '00'
 }
 
 
 
 function updateTimer(){
-    let milliElasped = Date.now() - startTime;
+    let millisElapsed = savedTime + Date.now() - startTime;
+    let secondsElapsed = Math.floor(millisElapsed / 1000);
+    let minutesElapsed = Math.floor(secondsElapsed / 60);
 
-    timerMilliseconds.innerHTML = millisElapsed
+    let minutesText = Math.floor(minutesElapsed);
+    let secondsText = Math.floor(secondsElapsed);
+    let millisText = millisElapsed % 1000;
+
+    if (minutesText.toString().length === 1){
+        minutesText ='0 '+ minutesText;
+    }
+    if (secondsText.toString().length === 1){
+        secondsText ='0 '+ secondsText;
+    }
+
+    if (millisText.toString().length < 3){
+     millisText = millisText.toString().padStart(3, '0');
+    }
+
+    timerMilliseconds.innerHTML = millisText;
+    timerSeconds.innerHTML = secondsText
+    timerMinutes.innerHTML = minutesText
 cancelId = requestAnimationFrame(updateTimer);
 
 }
